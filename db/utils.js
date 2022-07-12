07b81a5ac578754a055b2bb3f028bcb283bb7822
Auxane <auxane.nith@protonmail.com>
2022-07-13 00:12:33 +0200

    Clean git

--- db/utils.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.connectDB = void 0;
-const mongoose_1 = require("mongoose");
-const connectDB = (url) => {
-    return (0, mongoose_1.connect)(url);
-};
-exports.connectDB = connectDB;
-//# sourceMappingURL=utils.js.map
--- middleware/isAuthenticated.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const passport = require('passport');
-require('../utils/passport')(passport);
-module.exports = passport.authenticate('jwt', { session: false });
-//# sourceMappingURL=isAuthenticated.js.map
